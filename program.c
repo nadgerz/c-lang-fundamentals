8091e22b3c95245b564a6828f28d39bb8b4ad774
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-29 15:00:02 +0200

    oranges overwrote the apples value

--- program.c
+++ program.c
@@ -3,11 +3,26 @@
 void apples_set(int value);
 int apples_get();
 
+int data;
+int data;
+
+void oranges_set(int value)
+{
+    data = value;
+}
+
+int oranges_get()
+{
+    return data;
+}
+
+
 int main()
 {
     apples_set(5);
+    oranges_set(10);
 
-    printf("apples=%d\n", apples_get());
+    printf("apples=%d oranges=%d\n", apples_get(), oranges_get());
 
     return 0;
 }
