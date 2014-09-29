f2eb3172546c9edc7eef83152ff08ce80ea81186
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2014-09-29 14:50:26 +0200

    start to use set/get and a global variable

--- program.c
+++ program.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 
+int data;
+
+void apples_set(int value)
+{
+    data = value;
+}
+
+int apples_get()
+{
+    return data;
+}
+
+void oranges_set(int value)
+{
+    data = value;
+}
+
 int main()
 {
-    int apples = 5;
-    int oranges = apples;
+    int apples;
+    int oranges;
 
-    apples = 4;
+    apples_set(5);
+    apples = data;
+
+    oranges_set(apples);
+    oranges = data;
+
+    apples_set(4);
+    apples = data;
 
     printf("apples=%d oranges=%d\n", apples, oranges);
 
